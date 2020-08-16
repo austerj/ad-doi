32c8330c04860a16033cfbd2731b019993a52e13
austerj <johanauster@gmail.com>
2020-08-16 08:12:19 +0200

    direct sum in multivariate diffop

--- src/multivariate_heston/diffop.jl
+++ src/multivariate_heston/diffop.jl
@@ -1,16 +1,33 @@
 @muladd function diffop(t, s, ν, model::MultivariateHeston, contract::AbstractContract)::Float64
-    @unpack ξ, ρ = model
+    @unpack ξ, ρ, cholesky, ndims = model
 
     Hsν, Hνν = sensitivities(t, s, ν, model, contract)
 
-    Ds = Diagonal(s)
-    Dξ = Diagonal(ξ)
+    # matrix implementation - significantly slower than direct sum
+    # Ds = Diagonal(s)
+    # Dξ = Diagonal(ξ)
 
-    d = length(s)
-    ρsν = @view ρ[d+1:end, 1:d]
-    ρνν = @view ρ[d+1:end, d+1:end]
+    # d = length(s)
+    # ρsν = @view ρ[d+1:end, 1:d]
+    # ρνν = @view ρ[d+1:end, d+1:end]
 
-    transpose(.√ν) * (Ds*(ρsν.*Hsν) + 0.5*Dξ*(ρνν.*Hνν)) * Dξ*.√ν
+    # transpose(.√ν) * (Ds*(ρsν.*Hsν) + 0.5*Dξ*(ρνν.*Hνν)) * Dξ*.√ν
+
+    ddiffop = 0
+    for i=1:ndims, j=1:ndims
+        factor = ξ[i]*√(ν[i]*ν[j])
+        term_sν = s[i]*Hsν[i,j]
+        term_νν = .5*ξ[i]*Hνν[i,j]
+
+        sum_k = 0
+        for k=1:ndims
+             sum_k += term_sν*cholesky[k+i,j] + term_νν*cholesky[k+i,k+j]
+        end
+
+        ddiffop += factor * sum_k
+    end
+
+    ddiffop
 end
 
 function sensitivities(t, s, ν, model::MultivariateHeston, contract::AbstractContract)
